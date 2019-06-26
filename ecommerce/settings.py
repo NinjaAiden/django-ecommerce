44c4527557bf56cc22800d7b301c1edd1ae110cb
Aiden <aidenharvey102@gmail.com>
2019-06-26 12:23:19 +0000

    added search functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
