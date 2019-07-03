d6b64cf7c2d2d851462890fbfa93d98b0c226f42
Aiden <aidenharvey102@gmail.com>
2019-07-03 12:18:24 +0000

    added custom javascript for stripe API

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import OrderForm, MakePaymentForm
+from .forms import MakePaymentForm, OrderForm
 from .models import OrderLineItem
 from django.conf import settings
 from django.utils import timezone
 from products.models import Product
 import stripe
+
 
 # Create your views here.
 
@@ -29,12 +30,12 @@ def checkout(request):
                 product = get_object_or_404(Product, pk=id)
                 total += quantity * product.price
                 order_line_item = OrderLineItem(
-                    order = order,
-                    product = product,
+                    order = order, 
+                    product = product, 
                     quantity = quantity
                     )
                 order_line_item.save()
-            
+                
             try:
                 customer = stripe.Charge.create(
                     amount = int(total * 100),
@@ -44,7 +45,7 @@ def checkout(request):
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-            
+                
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
@@ -53,9 +54,9 @@ def checkout(request):
                 messages.error(request, "Unable to take payment")
         else:
             print(payment_form.errors)
-            messages.error(request, "We are unable to take payment with that card!")
+            messages.error(request, "We were unable to take a payment with that card!")
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
-    
+        
     return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
